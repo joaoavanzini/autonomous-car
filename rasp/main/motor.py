88cb5c9f3a7657e9b76e9c573c47175b237e1cfc
João Victor P. B. Avanzini <falejoaovictor@hotmail.com>
2023-09-06 21:34:35 -0300

    cleanup_pwm

--- rasp/main/motor.py
+++ rasp/main/motor.py
@@ -6,23 +6,27 @@ class Motor:
     def __init__(self, pwm_pin):
         self.pwm_pin = pwm_pin
         self.pwm = None
-        self.setup_pwm()
 
     def setup_pwm(self):
-        # Clean up any existing PWM object for the GPIO channel
+        # Check if a PWM object already exists for this GPIO channel
+        if self.pwm is None:
+            # Set up the GPIO channel as an output
+            GPIO.setup(self.pwm_pin, GPIO.OUT)
+
+            # Create a new PWM object
+            self.pwm = GPIO.PWM(self.pwm_pin, PWM_FREQUENCY)
+            self.pwm.start(0)
+
+    def cleanup_pwm(self):
+        # Clean up the existing PWM object
         if self.pwm is not None:
             self.pwm.stop()
             self.pwm = None
 
-        # Set up the GPIO channel as an output
-        GPIO.setup(self.pwm_pin, GPIO.OUT)
-
-        # Create a new PWM object
-        self.pwm = GPIO.PWM(self.pwm_pin, PWM_FREQUENCY)
-        self.pwm.start(0)
-
     def set_speed(self, speed):
+        self.setup_pwm()  # Ensure PWM setup before changing speed
         self.pwm.ChangeDutyCycle(speed)
 
     def stop(self):
+        self.setup_pwm()  # Ensure PWM setup before stopping
         self.pwm.ChangeDutyCycle(0)

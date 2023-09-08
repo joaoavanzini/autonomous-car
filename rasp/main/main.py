47b2be1488dabdab6ab38f05e0b6e7319eba3b80
João Victor P. B. Avanzini <falejoaovictor@hotmail.com>
2023-09-08 12:45:05 -0300

    Multiprocessing task to control and read sensor

--- rasp/main/main.py
+++ rasp/main/main.py
@@ -2,19 +2,19 @@
 from config import mqtt_client
 from rover import Rover
 from mqtt_client import MQTTClient
-import serial
-import json
+import multiprocessing
 from ultrasonic_sensor_data_reader import read_sensor_data
 
 
-if __name__ == "__main__":
+def main():
     rover = Rover()
     mqtt_client = MQTTClient()
     mqtt_client.connect()
     mqtt_client.subscribe()
     mqtt_client.start()
 
-    mqtt_client.publish_ultrasonic_data(read_sensor_data("/dev/ttyACM0"))
+    ultrasonic_process = multiprocessing.Process(target=read_ultrasonic_data, args=(mqtt_client,))
+    ultrasonic_process.start()
 
     try:
         while True:
@@ -22,3 +22,14 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         mqtt_client.client.disconnect()
         rover.cleanup_gpio()
+        ultrasonic_process.terminate()
+
+
+def read_ultrasonic_data(mqtt_client):
+    while True:
+        ultrasonic_data = read_sensor_data("/dev/ttyACM0")
+        if ultrasonic_data is not None:
+            mqtt_client.publish_ultrasonic_data(ultrasonic_data)
+
+if __name__ == "__main__":
+    main()

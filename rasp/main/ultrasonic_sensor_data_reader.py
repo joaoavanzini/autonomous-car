8c29cda21f10273d6d33f7a71152506b33dd101d
João Victor P. B. Avanzini <falejoaovictor@hotmail.com>
2023-09-08 17:30:06 -0300

    Performing MQTT connection

--- rasp/main/ultrasonic_sensor_data_reader.py
+++ rasp/main/ultrasonic_sensor_data_reader.py
@@ -4,26 +4,27 @@ import json
 from config import MQTT_BROKER_HOST, MQTT_DATA_SENSORS_TOPIC
 from mqtt_client import MQTTClient
 
-def read_sensor_data(serial_port, mqtt_client):
+def read_sensor_data(serial_port):
     try:
         with serial.Serial(serial_port, baudrate=9600) as ser:
             print(f"Serial connection to {serial_port} - open")
 
-            buffer = b""  # Inicialize um buffer vazio para armazenar os dados
+            buffer = b""
 
             while True:
-                data = ser.read(1)  # Leia um byte de cada vez
-                if data == b'{':  # Se encontrar um '{', comece a coletar os dados
-                    buffer = b"{"  # Inicialize o buffer com '{'
-                elif buffer and data == b'\n':  # Se encontrar uma nova linha
+                data = ser.read(1)
+                if data == b'{':
+                    buffer = b"{"
+                elif buffer and data == b'\n':
                     try:
-                        # Tente interpretar o buffer como JSON e, se bem-sucedido, publique-o
                         json_data = buffer.decode()
+                        print(json_data)
+                        mqtt_client = MQTTClient()
                         mqtt_client.publish_ultrasonic_data(json_data)
                     except Exception as e:
                         print(f"Error publishing ultrasonic data: {str(e)}")
-                    buffer = b""  # Limpe o buffer
-                elif buffer:  # Se o buffer estiver sendo preenchido, adicione os dados a ele
+                    buffer = b""
+                elif buffer:
                     buffer += data
 
     except Exception as e:

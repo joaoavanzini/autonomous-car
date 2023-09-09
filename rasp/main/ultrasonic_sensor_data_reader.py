04bad62408a0228d49fd2251cdd1ec0d290b3e9c
João Victor P. B. Avanzini <falejoaovictor@hotmail.com>
2023-09-09 11:59:09 -0300

    Trying to send the MQTT data sensors

--- rasp/main/ultrasonic_sensor_data_reader.py
+++ rasp/main/ultrasonic_sensor_data_reader.py
@@ -1,10 +1,9 @@
 # ultrasonic_sensor_data_reader.py
 import serial
 import json
-from config import MQTT_BROKER_HOST, MQTT_DATA_SENSORS_TOPIC
-from mqtt_client import MQTTClient
+from config import MQTT_BROKER_HOST, MQTT_DATA_SENSORS_TOPIC, mqtt_client
 
-def read_ultrasonic_data(mqtt_client):
+def read_ultrasonic_data():
     try:
         with serial.Serial("/dev/ttyACM0", baudrate=9600) as ser:
             print("Serial connection - open")
@@ -19,7 +18,8 @@ def read_ultrasonic_data(mqtt_client):
                     try:
                         json_data = buffer.decode()
                         print(json_data)
-                        mqtt_client.publish_ultrasonic_data(json.dumps(json_data))
+                        # Use mqtt_client para publicar os dados
+                        mqtt_client.publish(MQTT_DATA_SENSORS_TOPIC, json_data)
                     except Exception as e:
                         print(f"Error publishing ultrasonic data: {str(e)}")
                     buffer = b""

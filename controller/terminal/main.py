f3067d1da9dedfbce83503ed0ee65055d85f6359
João Victor P. B. Avanzini <falejoaovictor@hotmail.com>
2023-09-11 17:04:17 -0300

    Generated a user id

--- controller/terminal/main.py
+++ controller/terminal/main.py
@@ -1,17 +1,20 @@
-#controlller.py
 import paho.mqtt.publish as mqtt_publish
 from pynput import keyboard
+import uuid
 
 # MQTT Configuration
-MQTT_BROKER_HOST = "192.168.0.104"
-MQTT_TOPIC_CONTROLLER = "/rover/controller"
+MQTT_BROKER_HOST = "192.168.0.105"
+MQTT_TOPIC_CONTROLLER = "/controller"
 
-# Define the MQTT message payload for each key, including speed
+# Generate a random user ID
+user_id = str(uuid.uuid4())
+
+# Define the MQTT message payload for each key, including speed and user ID
 KEY_MAPPINGS = {
-    keyboard.Key.up: '{"direction": "BACKWARD", "speed": 100}',
-    keyboard.Key.down: '{"direction": "FORWARD", "speed": 100}',
-    keyboard.Key.left: '{"direction": "LEFT", "speed": 100}',
-    keyboard.Key.right: '{"direction": "RIGHT", "speed": 100}'
+    keyboard.Key.up: f'{{"user_id": "{user_id}", "direction": "BACKWARD", "speed": 100}}',
+    keyboard.Key.down: f'{{"user_id": "{user_id}", "direction": "FORWARD", "speed": 100}}',
+    keyboard.Key.left: f'{{"user_id": "{user_id}", "direction": "LEFT", "speed": 100}}',
+    keyboard.Key.right: f'{{"user_id": "{user_id}", "direction": "RIGHT", "speed": 100}}'
 }
 
 def on_key_press(key):
@@ -22,11 +25,10 @@ def on_key_press(key):
 
 def on_key_release(key):
     if key in KEY_MAPPINGS:
-        payload = '{"direction": "STOP"}'
+        payload = f'{{"user_id": "{user_id}", "direction": "STOP"}}'
         mqtt_publish.single(MQTT_TOPIC_CONTROLLER, payload=payload, hostname=MQTT_BROKER_HOST)
         print(f"Published: {payload}")
 
 # Start listening to keyboard events
 with keyboard.Listener(on_press=on_key_press, on_release=on_key_release) as listener:
     listener.join()
-
